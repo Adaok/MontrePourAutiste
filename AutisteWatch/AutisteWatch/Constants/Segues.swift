eecb2079cdc1341a8c44368ec69b1851ef363695
elacroix74 <etienne.lac74@gmail.com>
2016-06-01 14:59:38 +0200

    finalize Main Storyboard V1 Creating view controllers classes and segues name constants

---
+++ AutisteWatch/AutisteWatch/Constants/Segues.swift
@@ -0,0 +1,28 @@
+//
+//  Segues.swift
+//  AutisteWatch
+//
+//  Created by iem on 01/06/2016.
+//  Copyright © 2016 LP-BG-IEM. All rights reserved.
+//
+
+import Foundation
+
+struct Segues {
+    // MARK: - Activities Tab
+    let toAddActivity = "toAddActivity"
+    let toEditActivity = "toEditActivity"
+    
+    // MARK: - Groups Tab
+    let toPlanGroup = "toPlanGroup"
+    let toAddGroup = "toAddGroup"
+    
+    // MARK: - Patients Tab
+    let toPlanPatient = "toPlanPatient"
+    let toAddPatient = "toAddPatient"
+    
+    // MARK: - Multiple Tabs
+    let toPlanning = "toPlanning"
+    let toAddPlannedActivity = "toAddPlannedActivity"
+    let toEditPlannedActivity = "toEditPlannedAcitvity"
+}
