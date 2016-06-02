91a667a973599fc826503901b3afc754534d4a82
eddy <eddy.weissend@outlook.com>
2016-06-02 11:06:37 +0200

    add cells identifiers and added functionality to patientListViewController

--- AutisteWatch/AutisteWatch/View Controllers/Patients/PatientsListViewController.swift
+++ AutisteWatch/AutisteWatch/View Controllers/Patients/PatientsListViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class PatientsListViewController: UITableViewController {
 
+    var manager = PatientManager()
+    
+    var patients = [Patient]()
+    
+    lazy var addPatientButton : UIBarButtonItem = {
+        var button = UIBarButtonItem()
+        button.title = "+"
+        button.target = self
+        button.action = #selector(PatientsListViewController.addPatientItemAction)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        manager.createPatient("Jean-Eude", idWatch: "toto", group: nil)
+        patients = manager.fetchPatients()!
+        
+        self.clearsSelectionOnViewWillAppear = false
+        print()
+        self.navigationController!.title = "Patients"
+        self.navigationController!.navigationItem.setRightBarButtonItem(addPatientButton, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,61 +42,49 @@ class PatientsListViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return patients.count
     }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
+    
+    // MARK: - Table View Delegate
+    
+    //Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+ 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
 
     }
-    */
-
-    /*
+    
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PatientCell", forIndexPath: indexPath)
+        ((cell.viewWithTag(1)) as! UILabel).text = patients[indexPath.row].namePatient
+        return cell
+    }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -90,6 +92,14 @@ class PatientsListViewController: UITableViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
+    
+    // MARK: - Business
+    
+    func addPatientItemAction(){
+        
+    }
+    
+    
 
 }
