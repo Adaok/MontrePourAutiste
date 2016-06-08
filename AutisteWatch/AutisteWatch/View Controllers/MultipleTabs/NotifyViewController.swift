4bb5ab4a99e1d928dda38ad7ae1c9339eb2177e1
Loiodice Marco <mloiodice13@gmail.com>
2016-06-08 12:05:19 +0200

    update storyboard

--- AutisteWatch/AutisteWatch/View Controllers/MultipleTabs/NotifyViewController.swift
+++ AutisteWatch/AutisteWatch/View Controllers/MultipleTabs/NotifyViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class NotifyViewController: UIViewController {
     
+    @IBOutlet weak var imgVw_pictoNotify: UIImageView!
+    
+    var validateButton = UIBarButtonItem()
+
     var patientToNotify : Patient?
     var groupToNotify : Group?
     var isPatient : Bool = false
@@ -18,13 +22,21 @@ class NotifyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //validateButton = UIBarButtonItem(title: "Valider", style: .Done, target: self, action:Selector("validateNotification:"))
+        
+        var imageView = imgVw_pictoNotify
+    
         if isPatient {
             print(patientToNotify!.namePatient)
         }
         if isGroup {
             print(groupToNotify!.nameGroup)
         }
-
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("imageTapped:"))
+        imageView.userInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+        
         // Do any additional setup after loading the view.
     }
 
@@ -43,5 +55,16 @@ class NotifyViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - Actions
+    
+    func imageTapped(img: AnyObject)
+    {
+        print("toto")
+    }
+    
+    func validateNotification(notification: AnyObject){
+        print("sented")
+    }
 
 }
